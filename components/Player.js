890a81df5fe7c4df996e39771ead1776d33000fa
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2023-05-26 21:43:18 +0530

    Volume up down func

--- components/Player.js
+++ components/Player.js
@@ -2,7 +2,7 @@ import { useSession } from "next-auth/react";
 import useSpotify from "../hooks/useSpotify";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import { useRecoilState } from "recoil";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import useSongInfo from "../hooks/useSongInfo";
 import {
   ArrowPathIcon,
@@ -17,6 +17,7 @@ import {
   HeartIcon,
   SpeakerWaveIcon as VolumeDownIcon,
 } from "@heroicons/react/24/outline";
+import { debounce } from "lodash";
 
 function Player() {
   const spotifyApi = useSpotify();
@@ -59,6 +60,19 @@ function Player() {
     }
   }, [currentTrackId, spotifyApi, session]);
 
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]);
+
+  const debouncedAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => {});
+    }, 500),
+    []
+  );
+
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       <div className="flex items-center space-x-4">
@@ -89,6 +103,24 @@ function Player() {
         )}
         <ForwardIcon className="button" />
         <ArrowUturnLeftIcon className="button" />
+      </div>
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
+        <input
+          className="w-14 md:w-48"
+          type="range"
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
+          min={0}
+          max={100}
+        />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
       </div>
     </div>
   );
